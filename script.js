72ec342a5741647db7108201baaead143a78d42d
7lek <chromo-t@mail.ru>
2022-06-28 16:30:42 +0600

    scriptjs.add some

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let number = 5;
-const leftBorderWidth = 1;
+const numerOfFilms = +prompt ("сколько фильмов вы уже просмотрели?", "");
 
-number = 10;
-console.log(number);
+ const personalMovieDB = {
+    count: numerOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+ };
+
+ const a = prompt ("Один из последних просмотренных фильмов?", ""),
+    b = prompt ("На сколько оцените его?", ""),
+    c = prompt ("Один из последних просмотренных фильмов?", ""),
+    d = ("На сколько оцените его?", "");
+
+    personalMovieDB.movies [a] = b;
+    personalMovieDB.movies [c] = d;
+
+    console.log( personalMovieDB)
